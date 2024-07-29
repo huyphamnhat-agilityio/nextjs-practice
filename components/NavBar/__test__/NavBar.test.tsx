40c591e7aee825e6b299f4fb1c895fb6211637c2
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2024-07-29 09:34:27 +0700

    test: add unit test for NavBar

--- components/NavBar/__test__/NavBar.test.tsx
+++ components/NavBar/__test__/NavBar.test.tsx
@@ -1,10 +1,41 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
+import userEvent from "@testing-library/user-event";
+
+// Components
 import NavBar from "..";
 
 describe("NavBar", () => {
+  const setup = () => render(<NavBar />);
+
   it("should render correctly", () => {
-    const { container } = render(<NavBar />);
+    const { container } = setup();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render correctly on mobile screen", async () => {
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: query == "(max-width: 767px)",
+      media: "",
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }));
+
+    const { container } = setup();
+
+    const menuBtn = screen.getByRole("button", {
+      name: /open menu/i,
+    });
+
+    await userEvent.click(menuBtn);
+
+    const closeBtn = screen.findByRole("button", {
+      name: /close menu/i,
+    });
+
+    await userEvent.click(await closeBtn);
 
     expect(container).toMatchSnapshot();
   });
