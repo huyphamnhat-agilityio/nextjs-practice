1675084b1a239ce6f0f8836da434af9cdeabe421
huy.phamnhat <huy.phamnhat@asnet.com.vn>
2024-08-07 09:22:52 +0700

    refactor: create env file for env variables, remove storybook token

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  env: {
-    MOCK_API: "https://66a9a2a4613eced4eba5c4aa.mockapi.io",
-    PAGINATION_API: "https://api.free-apis-2024.cunguyen.com/pagination",
-  },
   images: {
     remotePatterns: [
       {
